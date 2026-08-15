8b3c955ac0951688aec523895bf0e2ca8789d2d1
Drishti <drishtisharma712005@gmail.com>
2026-08-15 17:24:13 +0530

    Quick Action bar Improved

--- frontend/src/features/dashboard/components/QuickActions/QuickActions.jsx
+++ frontend/src/features/dashboard/components/QuickActions/QuickActions.jsx
@@ -1,20 +1,30 @@
 import React from "react";
 import "./QuickActions.css";
+import { useNavigate } from "react-router-dom";
 
 function QuickActions({ handleStartSession }) {
+  const navigate = useNavigate();
+
   return (
     <div className="quick-actions-panel">
       <h3 className="quick-actions-title">Quick Actions</h3>
 
       <div className="quick-actions-buttons">
-        <button className="action-btn">Create Classroom</button>
+        <button className="action-btn" onClick={() => navigate("/classrooms/create")}>
+          Create Classroom
+        </button>
 
         <button className="action-btn" onClick={handleStartSession}>
           Start Session
         </button>
 
-        <button className="action-btn">Create Quiz</button>
-        <button className="action-btn">Mark Attendance</button>
+        <button className="action-btn" onClick={() => navigate("/quizzes")}>
+          Quiz
+        </button>
+
+        <button className="action-btn" onClick={() => navigate("/attendance")}>
+          Attendance
+        </button>
       </div>
     </div>
   );
